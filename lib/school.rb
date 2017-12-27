895dfa5f11376a39f44cde6331b9236647f2b000
Andy Maskiell <maskiella@gmail.com>
2017-12-27 16:21:55 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,7 @@ class School
   end
 
   def add_student(name, grade)
-    if !(@roster.has_key?(grade))
+    if !(School.@roster.has_key?(grade))
       @roster[grade] = []
       @roster[grade] << name
     else
